80615429d6a6829ee13af287279c9b60b316397d
FaisalABR <faisalabubakar92@gmail.com>
2026-03-03 21:15:51 +0700

    feat(surah): Implement SurahRepository with FindAll and FindByID methods

---
+++ internal/repository/surah_repository.go
@@ -0,0 +1,81 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+
+	surah "quran-api-go/internal/domain/surah"
+)
+
+type SurahRepository struct {
+	db *sql.DB
+}
+
+func NewSurahRepository(db *sql.DB) surah.SurahRepository {
+	return &SurahRepository{
+		db: db,
+	}
+}
+
+func (s *SurahRepository) FindAll(ctx context.Context) ([]surah.Surah, error) {
+	query := `SELECT id, number, name_arabic, name_latin, name_transliteration, number_of_ayahs, revelation_type FROM surahs`
+
+	rows, err := s.db.QueryContext(ctx, query)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var surahs []surah.Surah
+
+	for rows.Next() {
+		var surah surah.Surah
+		if err := rows.Scan(
+			&surah.ID,
+			&surah.Number,
+			&surah.NameArabic,
+			&surah.NameLatin,
+			&surah.NameTransliteration,
+			&surah.NumberOfAyahs,
+			&surah.RevelationType,
+		); err != nil {
+			return nil, err
+		}
+
+		surahs = append(surahs, surah)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return surahs, nil
+}
+
+func (s *SurahRepository) FindByID(ctx context.Context, id int) (*surah.Surah, error) {
+	query := `SELECT 
+	id, number, name_arabic, name_latin, name_transliteration, number_of_ayahs, revelation_type 
+	FROM surahs
+	WHERE id = ?`
+
+	row := s.db.QueryRowContext(ctx, query, id)
+
+	var surah surah.Surah
+	err := row.Scan(
+		&surah.ID,
+		&surah.Number,
+		&surah.NameArabic,
+		&surah.NameLatin,
+		&surah.NameTransliteration,
+		&surah.NumberOfAyahs,
+		&surah.RevelationType,
+	)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil
+		}
+		return nil, err
+	}
+
+	return &surah, nil
+}
