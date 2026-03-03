edba90575e71a5955f291d9d0fb450c2b06fdd2a
Ikhwan A. Latif <ikhwanal235@gmail.com>
2026-03-03 14:59:15 +0700

    feat(response): Revise Test to make sure the timestamp is returning string

--- pkg/response/response_test.go
+++ pkg/response/response_test.go
@@ -1,12 +1,26 @@
 package response
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
+
+type bodyTimestamp struct {
+	Timestamp string `json:"timestamp"`
+}
+
+func testTimestamp(t *testing.T, timestamp string) {
+	_, err := time.Parse(time.RFC3339, timestamp)
+	if err != nil {
+		t.Logf("invalid timestamp format: %v", err)
+		t.Fail()
+	}
+}
 
 func TestSuccessResponse(t *testing.T) {
 	w := httptest.NewRecorder()
@@ -20,6 +34,16 @@ func TestSuccessResponse(t *testing.T) {
 		t.Logf("expected 200, got %d", w.Code)
 		t.Fail()
 	}
+
+	var body struct {
+		bodyTimestamp
+	}
+
+	if err := json.Unmarshal(w.Body.Bytes(), &body); err != nil {
+		t.Logf("invalid json %v", err)
+	}
+
+	testTimestamp(t, body.Timestamp)
 }
 
 func TestNotFoundResponse(t *testing.T) {
@@ -34,18 +58,52 @@ func TestNotFoundResponse(t *testing.T) {
 		t.Logf("expected 404, got %d", w.Code)
 		t.Fail()
 	}
+
+	var body struct {
+		Error string `json:"error"`
+		bodyTimestamp
+	}
+
+	if err := json.Unmarshal(w.Body.Bytes(), &body); err != nil {
+		t.Logf("invalid json %v", err)
+	}
+
+	if body.Error != msg {
+		t.Logf("expected %s,surah got %s", msg, body.Error)
+		t.Fail()
+	}
+
+	testTimestamp(t, body.Timestamp)
 }
 
 func TestBadRequestResponse(t *testing.T) {
 	w := httptest.NewRecorder()
 	ctx, _ := gin.CreateTestContext(w)
 
-	BadRequest(ctx, "invalid lang")
+	msg := "invalid lang"
+
+	BadRequest(ctx, msg)
 
 	if w.Code != http.StatusBadRequest {
 		t.Logf("expected 400, got %d", w.Code)
 		t.Fail()
 	}
+
+	var body struct {
+		Error string `json:"error"`
+		bodyTimestamp
+	}
+
+	if err := json.Unmarshal(w.Body.Bytes(), &body); err != nil {
+		t.Logf("invalid json %v", err)
+	}
+
+	if body.Error != msg {
+		t.Logf("expected %s,surah got %s", msg, body.Error)
+		t.Fail()
+	}
+
+	testTimestamp(t, body.Timestamp)
 }
 
 func TestInternalErrorResponse(t *testing.T) {
@@ -57,4 +115,14 @@ func TestInternalErrorResponse(t *testing.T) {
 	if w.Code != http.StatusInternalServerError {
 		t.Fatalf("expected 500, got %d", w.Code)
 	}
+
+	var body struct {
+		bodyTimestamp
+	}
+
+	if err := json.Unmarshal(w.Body.Bytes(), &body); err != nil {
+		t.Logf("invalid json %v", err)
+	}
+
+	testTimestamp(t, body.Timestamp)
 }
